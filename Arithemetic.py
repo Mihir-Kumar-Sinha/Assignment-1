f7dc99c655552a736b58905f6f9c2a3f6131cb36
Mihir Kumar Sinha <amazingms912@gmail.com>
2025-09-02 09:22:20 +0530

    pythoncode

---
+++ Arithemetic.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter your first number: "))
+n2 = int(input("Enter your second number: "))
+
+print("Addition: ",n1+n2)
+print("Subtraction: ",n1-n2)
+print("Multiplication: ",n1*n2)
+print("Division: ",n1/n2)
